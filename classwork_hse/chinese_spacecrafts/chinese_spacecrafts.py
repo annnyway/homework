5164ce46fbfb8c2944ff73e1fc5cc92346be4b36
annnyway <nuticbooms@gmail.com>
2018-10-16 00:12:23 +0300

    changes in chinese_spacecrafts

--- classwork_hse/chinese_spacecrafts/chinese_spacecrafts.py
+++ classwork_hse/chinese_spacecrafts/chinese_spacecrafts.py
@@ -2,9 +2,9 @@ import re
 
 with open('wiki_chinese_space_program.txt', 'r') as f:
     text = f.read()
-spacecrafts = re.findall("(«[А-Я].[а-я]+?[нуэ](-[0-9]{1,3})?»)", text)
+spacecrafts = re.findall("(«[А-Я].[а-я]+?[нуэ].+?(-[0-9]{1,3})?»)", text)
 
 for i, spacecraft in enumerate(spacecrafts):
     spacecrafts[i] = spacecraft[0]
 
-print(list(set(spacecrafts)))
+print(list(spacecrafts))
